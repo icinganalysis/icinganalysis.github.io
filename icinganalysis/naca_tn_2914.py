fe03bcbdd22f8da8552514d2e0f0f1a9c7aade9d
theepdinker <theepdinker@gmail.com>
2022-04-24 15:34:11 -0700

    naca_tn_2914.py

---
+++ icinganalysis/naca_tn_2914.py
@@ -0,0 +1,165 @@
+from icinganalysis.units_helpers import tf_to_k, FT_PER_M
+from icinganalysis.water_properties import (
+    calc_vapor_p,
+    L_EVAPORATION,
+    RATIO_MOLECULAR_WEIGHTS,
+)
+from icinganalysis.air_properties import CP_AIR, calc_altitude, calc_pressure
+from icinganalysis.iteration_helpers import solve_minimize_f
+from icinganalysis.markdown_table_helper import make_nice_width_markdown_table
+
+INCH_HG_PER_PA = 0.00029529980164712
+
+mach = 1.36
+
+# fmt: off
+_d_front = (
+    9.13, 404, 403,
+    9.76, 406, 401,
+)
+# fmt: on
+ps_front = [_ / INCH_HG_PER_PA for _ in _d_front[::3]]
+ts_expirement_front_r = _d_front[1::3]
+ts_analytical_front_r = _d_front[2::3]
+
+# fmt: off
+_d_back = (
+    9.41, 422, 422,
+    9.91, 418, 422,
+    3.91, float('nan'), 447,
+)
+# fmt: on
+ps_back = [_ / INCH_HG_PER_PA for _ in _d_back[::3]]
+ts_expirement_back_r = _d_back[1::3]
+ts_analytical_back_r = _d_back[2::3]
+
+
+def calc_pressure_coefficient(mach, pl_p):
+    return (pl_p - 1) / (0.7 * mach ** 2)
+
+
+def calc_pressure_coefficient_from(mach, vl_vo):
+    pl_po = (-((vl_vo * mach / 2.236) ** 2 - 1 - 0.2 * mach ** 2)) ** (1 / 0.286)
+    cp = calc_pressure_coefficient(mach, pl_po)
+    return cp
+
+
+def calc_t(p, mach, pressure_coefficient, r=0.85):
+    t_freeze = 273.15
+    pl = p * (1 + 0.7 * mach ** 2 * pressure_coefficient)
+    es = calc_vapor_p(t_freeze)
+    tl_toc_mach = (1 + 0.2 * mach ** 2) * r + (1 - r) * (pl / p) ** 0.286
+
+    def calc_t_diff(tk):
+        eo = calc_vapor_p(tk)
+        dt = (
+            tk * tl_toc_mach
+            - t_freeze
+            - 1
+            * RATIO_MOLECULAR_WEIGHTS
+            / CP_AIR
+            * L_EVAPORATION
+            * (es / (pl - es) - eo / (p - eo))
+        )
+        return abs(dt)
+
+    tk = solve_minimize_f(calc_t_diff, [213.15, 293.15])
+
+    return tk
+
+
+# fmt: off
+d_p10_table_1 = (  # mach, pl/p, tl_toc_mach term, toc_rankine
+    0.4, 0.97, 1.028, 486.6,
+    0.5, 0.95, 1.041, 483.8,
+    0.6, 0.929, 1.059, 480,
+    0.7, 0.902, 1.079, 475.6,
+)
+# fmt: on
+machs_table_1 = d_p10_table_1[::4]
+pl_ps_table_1 = d_p10_table_1[1::4]
+tl_terms_table_1 = d_p10_table_1[2::4]
+toc_rankines_table_1 = d_p10_table_1[3::4]
+
+# fmt: off
+d_p10_table_2 = (  # mach, pl/p, tl_toc_mach term, toc_rankine
+    0.848, -0.355, 0.820, 460, 472.5, 484,
+    0.935, -0.330, 0.789, 452, 466.5, 481.5,
+    1.11, -0.045, 0.961, 423.5, 440, 464,
+)
+# fmt: on
+
+if __name__ == "__main__":
+
+    print()
+    for p, ts, cp in zip(ps_front, ts_analytical_front_r, (0.205, 0.225)):
+        tk = calc_t(p, mach, cp)
+        print(p, calc_altitude(p) * FT_PER_M, ts, tk * 1.8)
+
+    print()
+    for p, ts, cp in zip(ps_back, ts_analytical_back_r, (-0.096, -0.125, 0.15)):
+        tk = calc_t(p, mach, cp)
+        print(p, calc_altitude(p) * FT_PER_M, ts, tk * 1.8)
+    print()
+
+    p = calc_pressure(15000 / FT_PER_M)
+    vl_vo = 1.139
+    d = []
+    for mach, pl_p, tl_term, t_rankine in zip(
+        machs_table_1, pl_ps_table_1, tl_terms_table_1, toc_rankines_table_1
+    ):
+        cp = calc_pressure_coefficient_from(mach, vl_vo)
+        pl_p_calc = 1 + 0.7 * mach ** 2 * cp
+        tk = calc_t(p, mach, cp, r=0.85)
+        print(mach, cp, (1 + 0.7 * mach ** 2 * cp), pl_p, tk * 1.8, t_rankine)
+        d.append((mach, pl_p, f"{pl_p_calc:.3f}", t_rankine, f"{tk * 1.8:.1f}"))
+
+    print("Table on Page 10")
+    print()
+    text = make_nice_width_markdown_table(
+        (
+            "Mach",
+            "Reported pl/po",
+            "Calculated pl/po",
+            "Reported Toc, R",
+            "Calculated Toc, R",
+        ),
+        d,
+    )
+    print(text)
+    print()
+    print()
+    d = []
+    for mach, cp, pl_po, tr_10000, tr_25000, tr_40000 in zip(
+        *(iter(d_p10_table_2),) * 6
+    ):
+        pass
+        tk_10000 = calc_t(calc_pressure(10000 / FT_PER_M), mach, cp, r=0.90)
+        tk_25000 = calc_t(calc_pressure(25000 / FT_PER_M), mach, cp, r=0.90)
+        tk_40000 = calc_t(calc_pressure(40000 / FT_PER_M), mach, cp, r=0.90)
+        d.append(
+            (
+                mach,
+                cp,
+                tr_10000,
+                f"{tk_10000 * 1.8:.1f}",
+                tr_25000,
+                f"{tk_25000 * 1.8:.1f}",
+                tr_40000,
+                f"{tk_40000 * 1.8:.1f}",
+            )
+        )
+    text = make_nice_width_markdown_table(
+        (
+            "Mach",
+            "Coefficient of pressure",
+            "10000 ft. Toc, R",
+            "Calculated Toc, R",
+            "25000 ft. Toc, R",
+            "Calculated Toc, R",
+            "40000 ft. Toc, R",
+            "Calculated Toc, R",
+        ),
+        d,
+    )
+    print(text)
