7232ac1ab043b10a02ffdb44a862cae3b5d00291
theepdinker <theepdinker@gmail.com>
2024-03-28 16:05:56 -0700

    standard_computational_model.py

---
+++ icinganalysis/intermediate/standard_computational_model.py
@@ -0,0 +1,312 @@
+from icinganalysis import water_properties
+from icinganalysis import air_properties
+from icinganalysis import compressible_flow
+from icinganalysis import units_helpers
+from icinganalysis import cyl_beta2_43
+from icinganalysis.langmuir_cylinder_values import calc_ko_d2
+
+
+T_MP = 273.15  # melting temperature of ice, K
+CP_AIR = air_properties.CP_AIR
+
+
+def calc_m_imp(u, lwc, beta):
+    """Equation 2-34
+    We will eschew the part
+    "However, if the local velocity at the edge of the boundary layer is
+    available, that velocity should be used rather than the freestream velocity"
+    as that is not how conventional Beta values work.
+    """
+    m_imp = u * lwc / units_helpers.G_PER_KG * beta
+    return m_imp
+
+
+def calc_t_film(t, t_surface=T_MP):
+    """
+
+    :param t:
+    :param t_surface:
+    :return:
+    """
+    t_film = (t + t_surface) / 2
+    # return t_surface
+    return t_film
+
+
+def calc_k_a(t_film):
+    """
+    Calculate the thermal conductivity of air
+    Equation A.3 in NASA/CR-2004-212875 implemented SI units
+    Symbol k_a
+    Note that the examples use t_film (average of static air temperature and t_surface)
+    :param t_film: temperature, K
+    :return: air thermal conductivity, W/m-K (J/s-m-K)
+    """
+    return -0.0147486 + 0.00235815 * t_film**0.5
+
+
+def calc_mu_a(t):
+    """
+    Calculate the dynamic viscosity of air
+    Equation A.24 in NASA/CR-2004-212875 implemented SI units
+    Symbol: mu_a
+    :param t: temperature, K
+    :return: viscosity, kg/m-s
+    """
+    return 1e-5 / (0.12764 + 124.38 / t)
+
+
+def calc_pr_a(mu_a, k_a):
+    """
+    Calculate the Prandtl number
+    Equation A.42 in NASA/CR-2004-212875 implemented SI units
+    Symbol: pr_a
+    :param mu_a: viscosity of air, kg/m-s
+    :param k_a: thermal conductivity of air, W/m-K
+    :return: Prandtl Number (dimensionless)
+    """
+    return CP_AIR * mu_a / k_a
+
+
+def calc_re_a(ms, d, rho_a, mu_a):
+    """
+    Calculate Reynolds number
+    Equation A.44 in NASA/CR-2004-212875 implemented SI units
+    Symbol: re_a
+    :param ms: airspeed, m/s
+    :param d: leading edge diameter of curvature, m
+    :param rho_a: air density, kg/m^3.
+    :param mu_a: air viscosity, kg/m-s
+    :return: Reynolds number (dimensionless)
+    """
+    return ms * d * rho_a / mu_a
+
+
+def calc_reynolds_from(ms, t, p, d):
+    """
+    Calculate the Reynold number for air
+    :param ms: airspeed, m/s
+    :param t: air static temperature, K
+    :param p: air static pressure, Pa
+    :param d: length, m
+    :return: Reynolds number for air
+    """
+    return calc_re_a(ms, d, air_properties.calc_air_density(t, p), calc_mu_a(t))
+
+
+def calc_d_v(t_film, p_st):
+    """
+    Calculate the thermal diffusivity of water vapor in air
+    Equation A.4 in NASA/CR-2004-212875 implemented SI units
+    Symbol d_v
+    Note that the examples use t_film (average of static air temperature and t_surface)
+    :param t_film: temperature, K
+    :param p_st: air static pressure, Pa (N/m^2)
+    :return: diffusivity, m^2/s
+    """
+    return 0.211 / 100**2 * (t_film / 273.15) ** 1.94 * (101320 / p_st)
+
+
+def calc_schmidt(mu_a, rho_a, d_v):
+    """
+    Calculate the Schmidt number
+    Equation A.46 in NASA/CR-2004-212875 implemented SI units
+    Symbol: s_c_a
+        :param mu_a: air viscosity, kg/m-s
+    :param rho_air: air density, kg/m^3.
+    :param d_v: diffusivity of water vapor in air, m^2/s
+    :return: Schmidt number (dimensionless)
+    """
+    return mu_a / (rho_a * d_v)
+
+
+def calc_hc_o_cylinder_from(u, t, diameter, p):
+    """
+    Calculate the heat transfer coefficient at the stagnation line of a cylinder
+    :param u: airspeed, m/s
+    :param t: air static temperature, K
+    :param diameter: significant length, m (diameter of curvature)
+    :param p: air static pressure, Pa
+    :return: heat transfer coefficient at the stagnation line of a cylinder, W/m^2-K
+    """
+    t_film = calc_t_film(t)
+    pr = calc_pr_a(calc_mu_a(t_film), calc_k_a(t_film))
+    re_a = calc_reynolds_from(u, t_film, p, diameter)
+    nu_a = (
+        1.14 * pr**0.4 * re_a**0.5
+    )  # From Kreith, "Principles of Heat Transfer" for a laminar cylinder leading edge
+    hc = calc_k_a(t_film) * nu_a / diameter
+    return hc
+
+
+def calc_q_aero_heat(u, hc, rc=0.9):
+    """Equation 2-38"""
+    q_aero_heat = hc * rc * u**2 / (2 * CP_AIR)
+    return q_aero_heat
+
+
+def calc_q_conv(hc, t, t_surface=T_MP):
+    """Equation 2-42"""
+    q_conv = hc * (t_surface - t)
+    return q_conv
+
+
+def calc_q_conv2(hc, tr, t_surface=T_MP):
+    """Equation 2-42b"""
+    q_conv2 = hc * (t_surface - tr)
+    return q_conv2
+
+
+def calc_tr(t, hc, rc, u):
+    """Equation 2-42c"""
+    tr = t + hc * rc * u**2 / (2 * CP_AIR)
+    return tr
+
+
+def calc_q_drop_warm(m_imp, t, t_surf=T_MP):
+    """Equation 2-43"""
+    q_drop_warm = m_imp * water_properties.WATER_SPECIFIC_HEAT * (t_surf - t)
+    return q_drop_warm
+
+
+def calc_q_ice_cool(n, m_imp, t_surf=T_MP):
+    """
+    Equation 2-40
+
+    I believe that there is a typo in the source (both the 1991 and 1193 update).
+    The product n*M"ice does not make sense.
+    Either n*M"imp or M"ice would make sense.
+    n*M"imp is the form used in NASA-CR-2004-212875, Section 3.5, term (9).
+
+    :param n:
+    :param m_imp:
+    :param t_surf:
+    :return:
+    """
+    m_ice = m_imp * n
+    q_ice_cool = m_ice * water_properties.ICE_SPECIFIC_HEAT * (T_MP - t_surf)
+    return q_ice_cool
+
+
+def calc_m_evap(t, p, hc, u, t_surf=T_MP, po=None, to=None):
+    if po is None:
+        po = p
+    if to is None:
+        to = t
+    t_film = calc_t_film(t)
+    schmidt = calc_schmidt(
+        calc_mu_a(t_film),
+        air_properties.calc_air_density(t_film, p),
+        calc_d_v(t_film, p),
+    )
+    g = (
+        hc
+        / CP_AIR
+        * (calc_pr_a(calc_mu_a(t_film), calc_k_a(t_film)) / schmidt) ** 0.667
+    )
+    b1 = (
+        water_properties.calc_vapor_p(t_surf) / t
+        - po / p * water_properties.calc_vapor_p(t) / to
+    )
+    b2 = 1 / 0.622 * po / to - water_properties.calc_vapor_p(t_surf) / t
+    delta_b = b1 / b2
+    m_evap = g * delta_b
+    return m_evap
+
+
+def calc_q_evap(m_evap):
+    q_evap = m_evap * water_properties.L_EVAPORATION
+    return q_evap
+
+
+def calc_q_drop_ke(m_imp, u):
+    q = m_imp * u**2 / 2
+    return q
+
+
+def calc_energy_and_mass_balance(
+    t, p, u, lwc, hc, beta, m_run_in=0, t_surf=T_MP, po=None, to=None, rc=0.85
+):
+    if po is None:
+        po = p
+    if to is None:
+        to = t
+    m_imp = calc_m_imp(u, lwc, beta)
+    m_incoming = m_imp + m_run_in
+    q_aero_heat = calc_q_aero_heat(u, hc, rc)
+    q_drop_ke = calc_q_drop_ke(m_imp, u)
+    q_conv = calc_q_conv(hc, t, t_surf)
+    q_drop_warm = calc_q_drop_warm(m_imp, t, t_surf)
+    m_evap = calc_m_evap(t, p, hc, u, t_surf, po, to)
+    m_evap = min(m_incoming, m_evap)
+    q_evap = calc_q_evap(m_evap)
+    q_sink = q_conv + q_drop_warm + q_evap
+    q_freeze = -(q_aero_heat + q_drop_ke - q_sink)
+    m_freeze = q_freeze / water_properties.L_FREEZING
+    q_source = q_freeze + q_drop_ke + q_aero_heat
+    if m_incoming == 0:
+        n = 0
+        if m_freeze > 0:
+            n = 1
+    else:
+        n = m_freeze / m_incoming
+    return (
+        n,
+        q_aero_heat,
+        q_freeze,
+        q_drop_ke,
+        q_conv,
+        q_evap,
+        q_drop_warm,
+    )
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import simple_csv_reader
+
+    f = r"tables_2_5_to_8.csv"
+
+    header, vs = simple_csv_reader.simple_csv_reader(f)
+
+    p = air_properties.calc_pressure(0)
+    diameter = 0.2  # m, 20 cm
+
+    ws = []
+
+    for case, u, mvd, lwc, tc, n, pqv, pqf, pqk, pqc, pqe, pqw in vs:
+        t = units_helpers.tc_to_k(tc)
+        hc = calc_hc_o_cylinder_from(u, t, diameter, p)
+        beta = cyl_beta2_43.get_beta(calc_ko_d2(t, p, u, mvd, diameter))
+        cp = 1
+        mach = compressible_flow.calc_mach(u, t)
+        po = p * (0.7 * cp * mach**2 + 1)
+        to = t * (p / po) ** (1 / 3.5)
+
+        (
+            n_calc,
+            q_aero_heat,
+            q_freeze,
+            q_drop_ke,
+            q_conv,
+            q_evap,
+            q_drop_warm,
+        ) = calc_energy_and_mass_balance(t, p, u, lwc, hc, beta, po=po, to=to)
+        n_calc = min(float(n_calc), 1)
+
+        ws.append((case, tc, n, n_calc, beta))
+
+    ws = list(ws)
+    case, tc, n, n_calc, beta = list(zip(*ws))
+
+    plt.figure()
+    plt.suptitle("Handbook Table 2-5 to 2-8 data")
+    plt.plot((0, 1), (0, 1), "--", label="Perfect agreement")
+    plt.plot(n, n_calc, "+", label="calculated points")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1.1)
+    plt.xlabel("Freezing fraction reference, n")
+    plt.xlabel("Freezing fraction calculated")
+    plt.legend()
+
+    plt.show()
---
+++ icinganalysis/intermediate/table_2_5.py
@@ -0,0 +1,200 @@
+from icinganalysis import units_helpers
+from icinganalysis import air_properties
+from icinganalysis.langmuir_cylinder_values import (
+    langmuir_lwc_fractions,
+    get_mids,
+)
+from icinganalysis.intermediate import standard_computational_model
+from icinganalysis import compressible_flow
+
+hc_calc_method = standard_computational_model.calc_hc_o_cylinder_from
+
+
+def calc_cylinder_beta_max_standard_computational_model_with_distribution(
+    t,
+    p,
+    u,
+    mvd,
+    diameter,
+    distribution="Langmuir A",
+):
+    d_mids = get_mids(distribution)
+    beta = 0
+    for r, w in zip(d_mids, langmuir_lwc_fractions):
+        ko = standard_computational_model.calc_ko_d2(t, p, u, r * mvd, diameter)
+        if ko == ko and ko > 0:  # Filter out 'nan' and unrealistic Ko values
+            beta += w * max(0, standard_computational_model.cyl_beta2_43.get_beta(ko))
+    return beta
+
+
+def calc_cylinder_standard_computational_model_with_distribution(
+    t,
+    p,
+    u,
+    lwc,
+    mvd,
+    diameter,
+    distribution="Langmuir A",
+):
+    beta = calc_cylinder_beta_max_standard_computational_model_with_distribution(
+        t, p, u, mvd, diameter, distribution
+    )
+    hc = standard_computational_model.calc_hc_o_cylinder_from(u, t, diameter, p)
+    cp = 1
+    mach = compressible_flow.calc_mach(u, t)
+    po = p * (0.7 * cp * mach**2 + 1)
+    to = t * (p / po) ** (1 / 3.5)
+    (
+        n_calc,
+        q_aero_heat,
+        q_freeze,
+        q_drop_ke,
+        q_conv,
+        q_evap,
+        q_drop_warm,
+    ) = standard_computational_model.calc_energy_and_mass_balance(
+        t, p, u, lwc, hc, beta, po=po, to=to
+    )
+    return (n_calc, q_aero_heat, q_freeze, q_drop_ke, q_conv, q_evap, q_drop_warm, beta)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import simple_csv_reader
+
+    # DOT/FAA/CT-88/8-1
+    f = r"tables_2_5_to_8.csv"
+    header, vs = simple_csv_reader.simple_csv_reader(f)
+
+    p = air_properties.calc_pressure(0)
+    diameter = 0.2
+    ns = []
+    cases = []
+    n_scms = []
+    print()
+    print("DOT/FAA/CT-88/8-1 Tables 2-5 thru 2-8 cylinder cases")
+    print(r"                       Reference\calculated")
+    print("                                   Heat Sources         Heat Sinks")
+    print("Case   TC  V  LWC  MVD  n          %qv    %qf     %qk   %qc    %qe    %qw")
+    for vs_ in vs:
+        case, u, mvd, lwc, tc, N, PQV, PQF, PQK, PQC, PQE, PQW = vs_
+        cases.append(case)
+        ns.append(N)
+        tk = units_helpers.tc_to_k(tc)
+
+        (
+            n_scm,
+            q_aero_heat,
+            q_freeze,
+            q_drop_ke,
+            q_conv,
+            q_evap,
+            q_drop_warm,
+            beta,
+        ) = calc_cylinder_standard_computational_model_with_distribution(
+            tk, p, u, lwc, mvd, diameter, "Langmuir A"
+        )
+        heat_sources = q_drop_ke + q_freeze + q_aero_heat
+        sinks = q_conv + q_evap + q_drop_warm
+        pcts = (
+            q_aero_heat / heat_sources,
+            q_freeze / heat_sources,
+            q_drop_ke / heat_sources,
+            q_conv / sinks,
+            q_evap / sinks,
+            q_drop_warm / sinks,
+        )
+        try:
+            pcts = [
+                float(_[0])  # values are np.ndarray type, float for easy printing
+                for _ in pcts
+            ]
+        except Exception:
+            pass
+        n_scms.append(n_scm)
+        print(
+            f"{case:5s} {tc:3.0f} {u:3.0f} {lwc:.2f} {mvd:3.0f}  {N:.2f}\\{float(n_scm):.2f} ",
+            "".join(
+                [
+                    f"{_[1]:2.0f}\\{100 * _[0]:2.0f}  "
+                    for _ in zip(pcts, (PQV, PQF, PQK, PQC, PQE, PQW))
+                ]
+            ),
+        )
+
+    plt.figure(figsize=(8, 8))
+    plt.suptitle("DOT/FAA/CT-88/8-1 Tables 2-5 thru 2-8 Cylinder cases")
+    for n_, nc_, t_ in zip(ns, n_scms, cases):
+        plt.text(n_, nc_, t_)
+    plt.plot((0, 1), (0, 1), "--", label="Perfect agreement")
+    plt.plot(
+        ns,
+        n_scms,
+        "o",
+        fillstyle="none",
+        label="Standard Computational Model calculated points",
+    )
+    plt.xlabel("Reference freezing fraction n")
+    plt.ylabel("Calculated freezing fraction n")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1.1)
+    plt.grid()
+    plt.legend(loc="upper left")
+    plt.savefig("dot_faa_ct_88_8_1_cylinder_n.png")
+
+    """
+DOT/FAA/CT-88/8-1 Tables 2-5 thru 2-8 cylinder cases
+                       Reference\calculated
+                                   Heat Sources         Heat Sinks
+Case   TC  V  LWC  MVD  n          %qv    %qf     %qk   %qc    %qe    %qw
+2-5-a -26  70 0.70  20  0.90\0.91   5\ 4  94\95   1\ 1  48\49  16\16  36\34  
+2-5-a -20  70 0.70  20  0.70\0.71   6\ 5  93\94   1\ 1  47\48  18\18  35\33  
+2-5-a -14  70 0.70  20  0.49\0.50   8\ 7  91\92   1\ 1  46\47  21\21  33\32  
+2-5-a  -8  70 0.70  20  0.27\0.28  14\12  84\86   2\ 2  44\45  24\24  32\31  
+2-5-a  -2  70 0.70  20  0.03\0.03  53\48  38\43   9\ 9  44\46  25\23  31\32  
+2-5-b  -6  70 0.10  20  0.93\0.98  24\21  75\78   1\ 1  60\61  34\33   6\ 6  
+2-5-b  -4  70 0.10  20  0.53\0.57  36\32  63\68   1\ 1  59\61  35\33   6\ 6  
+2-5-b  -2  70 0.10  20  0.10\0.13  73\65  25\33   2\ 2  60\63  34\31   6\ 6  
+2-6-a -26  70 0.70  20  0.90\0.91   5\ 4  95\95   1\ 1  48\49  15\16  36\34  
+2-6-a -26  70 0.80  20  0.83\0.83   4\ 4  95\95   1\ 1  46\47  14\15  40\37  
+2-6-a -26  70 0.90  20  0.78\0.78   4\ 4  95\95   1\ 1  44\45  14\15  42\40  
+2-6-a -26  70 1.00  20  0.73\0.73   4\ 3  95\96   1\ 1  42\43  13\14  45\43  
+2-6-a -26  70 2.00  20  0.54\0.53   3\ 2  96\96   1\ 1  29\30   9\10  62\60  
+2-6-a -26  70 3.00  20  0.47\0.46   2\ 2  96\97   2\ 2  22\23   7\ 8  71\69  
+2-6-a -26  70 4.00  20  0.44\0.42   2\ 2  97\97   2\ 2  18\19   6\ 6  77\75  
+2-6-a -26  70 5.00  20  0.42\0.40   1\ 1  97\97   2\ 2  15\16   5\ 5  80\79  
+2-6-b  -6  70 0.10  20  0.93\0.98  24\21  75\78   1\ 1  60\61  34\33   6\ 6  
+2-6-b  -6  70 0.40  20  0.28\0.30  21\18  77\80   2\ 2  51\52  29\28  21\20  
+2-6-b  -6  70 0.70  20  0.19\0.20  18\15  79\81   3\ 3  44\45  25\24  32\31  
+2-6-b  -6  70 1.00  20  0.15\0.16  16\14  80\83   4\ 4  39\39  22\21  40\39  
+2-6-b  -6  70 1.30  20  0.14\0.14  14\12  82\83   4\ 4  34\35  19\19  46\46  
+2-6-b  -6  70 1.60  20  0.12\0.13  13\11  82\84   5\ 5  31\32  18\17  51\51  
+2-6-b  -6  70 1.90  20  0.11\0.12  12\10  83\85   5\ 5  28\29  16\16  56\55  
+2-6-b  -6  70 2.20  20  0.11\0.11  11\ 9  84\85   6\ 6  26\27  15\15  59\59  
+2-6-b  -6  70 2.50  20  0.10\0.10  10\ 9  84\85   6\ 6  24\25  14\13  62\62  
+2-6-b  -6  70 2.80  20  0.10\0.10   9\ 8  85\86   6\ 6  23\23  13\13  65\64  
+2-7-a -26  70 0.70  20  0.90\0.91   5\ 4  95\95   1\ 1  48\49  15\16  36\34  
+2-7-a -26  70 0.70  40  0.68\0.68   4\ 3  95\96   1\ 1  39\41  12\13  48\46  
+2-7-a -26  70 0.70  60  0.64\0.63   3\ 3  95\96   1\ 1  37\38  11\12  52\50  
+2-7-a -26  70 0.70  80  0.62\0.61   3\ 3  96\96   1\ 1  35\36  11\12  54\52  
+2-7-b  -6  70 0.10  20  0.93\0.98  24\21  75\78   1\ 1  60\61  34\33   6\ 6  
+2-7-b  -6  70 0.10  40  0.60\0.62  24\20  76\79   1\ 1  58\59  33\32  10\10  
+2-7-b  -6  70 0.10  60  0.52\0.55  23\20  76\79   1\ 1  57\58  32\31  11\11  
+2-7-b  -6  70 0.10  80  0.49\0.51  23\20  76\79   1\ 1  56\57  32\31  12\12  
+2-8-a -26  70 0.70  20  0.90\0.91   5\ 4  95\95   1\ 1  48\49  15\16  36\34  
+2-8-a -26  80 0.70  20  0.82\0.83   6\ 5  93\94   1\ 1  46\48  14\15  39\37  
+2-8-a -26  90 0.70  20  0.76\0.76   7\ 6  92\92   1\ 1  45\46  14\14  42\40  
+2-8-a -26 100 0.70  20  0.71\0.71   8\ 7  90\91   2\ 2  43\44  13\14  44\42  
+2-8-a -26 110 0.70  20  0.67\0.66  10\ 9  88\89   2\ 2  42\43  13\13  46\44  
+2-8-a -26 120 0.70  20  0.62\0.62  11\10  86\87   3\ 3  40\42  12\12  48\46  
+2-8-a -26 130 0.70  20  0.59\0.59  13\11  84\85   4\ 4  39\41  11\12  49\48  
+2-8-b  -6  70 0.10  20  0.93\0.98  24\21  75\78   1\ 1  60\61  34\33   6\ 6  
+2-8-b  -6  80 0.10  20  0.74\0.79  32\28  67\71   1\ 1  60\61  33\32   7\ 7  
+2-8-b  -6  90 0.10  20  0.58\0.63  40\35  58\64   1\ 1  60\61  32\31   8\ 8  
+2-8-b  -6 100 0.10  20  0.44\0.49  50\44  48\54   2\ 2  60\62  32\30   8\ 8  
+2-8-b  -6 110 0.10  20  0.31\0.37  61\54  37\44   2\ 2  60\63  30\28   9\ 9  
+2-8-b  -6 120 0.10  20  0.19\0.25  72\64  25\33   3\ 3  60\63  30\27  10\10  
+2-8-b  -6 130 0.10  20  0.08\0.14  85\76  11\20   3\ 4  61\64  29\25  10\11  
+    """
+
+    plt.show()
